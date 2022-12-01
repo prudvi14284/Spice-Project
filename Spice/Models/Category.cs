1bd2c6e4c426506b84680036cd1b26c93c4c7def
prudvi <prudvi14284@gmail.com>
2022-12-01 10:19:08 +0530

    Add project files.

---
+++ Spice/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spice.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Category")]
+        public String Name { get; set; }
+    }
+}
